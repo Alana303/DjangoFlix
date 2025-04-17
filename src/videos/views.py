bc61554dfc503062c23c940d4f61545603278629
Alana303 <alanaesmeralda045@gmail.com>
2025-04-17 16:28:20 +0300

    Customized DjangoFlix with UI updates, home & search pages, Bootstrap styling

--- src/videos/views.py
+++ src/videos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from playlists.models import Playlist  # Adjust if using another model
 
-# Create your views here.
+def home_view(request):
+    qs = Playlist.objects.all()[:6]
+    return render(request, "home.html", {"object_list": qs})
+
+def search_view(request):
+    query = request.GET.get("q")
+    qs = Playlist.objects.filter(title__icontains=query)
+    return render(request, "search.html", {"object_list": qs, "query": query})
